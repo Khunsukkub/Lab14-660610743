bcc489a874919bf0779913f61594e0c95b8cbb2c
Khun <khunsuk@eueu.com>
2024-01-05 16:38:03 +0700

    Updated

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -4,7 +4,29 @@ using namespace std;
 template <typename T>
 void insertionSort(T d[],int N){
 
+	for(int i = 1 ; i < N ; i++){
+
+		T unsofted = d[i];
+		int j;
+		for(j = i-1 ; j >= 0 ; j--){
+			if(unsofted > d[j]){
+				d[j+1] = d[j];
+			} else{
+				break;
+			}
+		}
+		d[j+1] = unsofted;
+		
+		cout << "Pass " << i << ":";
+		for(int omg = 0 ; omg < N ; omg++){
+			cout << d[omg] << " ";
+		}
+		cout << endl;
+
+	}
+
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
